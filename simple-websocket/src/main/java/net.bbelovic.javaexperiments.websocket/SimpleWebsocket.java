0016970f24e2bea07b3c8a8498c8986bee011210
bbelovic <bbelovic@gmail.com>
2018-08-22 22:03:32 +0200

    Implemented stop

--- simple-websocket/src/main/java/net.bbelovic.javaexperiments.websocket/SimpleWebsocket.java
+++ simple-websocket/src/main/java/net.bbelovic.javaexperiments.websocket/SimpleWebsocket.java
@@ -1,34 +1,46 @@
 package net.bbelovic.javaexperiments.websocket;
 
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 @ServerEndpoint("/simple")
 public class SimpleWebsocket {
     private static volatile AtomicLong cntId = new AtomicLong(0L);
     private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    private ScheduledFuture<?> future;
 
     @OnOpen
     public void startClock(Session session) {
+        final var id = cntId.incrementAndGet();
         Runnable r = () -> {
             LocalTime now = LocalTime.now();
-            String formatted = ISO_LOCAL_TIME.format(now);
+            var clientId = "Client-" + id;
+            var formatted = DateTimeFormatter.ISO_TIME.format(now);
             try {
-                session.getBasicRemote().sendText(formatted);
+                session.getBasicRemote().sendText(clientId +":"+ formatted);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         };
-        System.out.println("Starting clock - " + (cntId.incrementAndGet()) );
-        executorService.scheduleAtFixedRate(r, 0, 1, SECONDS);
+        this.future = executorService.scheduleAtFixedRate(r, 0, 1, SECONDS);
+    }
+
+    @OnMessage
+    public void stopClock(String message) {
+        if ("stop".equals(message)) {
+            this.future.cancel(true);
+            System.out.println("Stopped..");
+        }
     }
 }
