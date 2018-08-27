5c96f25721cbc5a912854d9e0a4b2beab37b54c7
bbelovic <bbelovic@gmail.com>
2018-08-27 22:41:19 +0200

    Toing with websockets

---
+++ simple-websocket/src/main/java/net.bbelovic.javaexperiments.websocket/ClockRunnable.java
@@ -0,0 +1,34 @@
+package net.bbelovic.javaexperiments.websocket;
+
+import javax.websocket.Session;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class ClockRunnable implements Runnable {
+    private final Session session;
+    private final  String clientId;
+
+    public ClockRunnable(Session session, String clientId) {
+        this.session = session;
+        this.clientId = clientId;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println("interrupted="+Thread.currentThread().isInterrupted());
+            LocalTime now = LocalTime.now();
+            var formatted = DateTimeFormatter.ISO_TIME.format(now);
+            try {
+                session.getBasicRemote().sendText(clientId + ":" + formatted);
+                Thread.sleep(1000L);
+            } catch (Exception e) {
+                System.out.println("ex interrupted="+Thread.currentThread().isInterrupted());
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                System.out.println("ex2 interrupted="+Thread.currentThread().isInterrupted());
+            }
+        }
+
+    }
+}
--- simple-websocket/src/main/java/net.bbelovic.javaexperiments.websocket/SimpleWebsocket.java
+++ simple-websocket/src/main/java/net.bbelovic.javaexperiments.websocket/SimpleWebsocket.java
@@ -4,43 +4,42 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicLong;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 @ServerEndpoint("/simple")
 public class SimpleWebsocket {
     private static volatile AtomicLong cntId = new AtomicLong(0L);
-    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-    private ScheduledFuture<?> future;
+    private volatile boolean running;
+    private Thread thread;
 
     @OnOpen
     public void startClock(Session session) {
         final var id = cntId.incrementAndGet();
         System.out.println(String.format("Client %d connected", id));
+        var clientId = "Client-" + id;
+        running = true;
         Runnable r = () -> {
-            LocalTime now = LocalTime.now();
-            var clientId = "Client-" + id;
-            var formatted = DateTimeFormatter.ISO_TIME.format(now);
-            try {
-                session.getBasicRemote().sendText(clientId +":"+ formatted);
-            } catch (IOException e) {
-                e.printStackTrace();
+            while (running) {
+                LocalTime now = LocalTime.now();
+                var formatted = DateTimeFormatter.ISO_TIME.format(now);
+                try {
+                    session.getBasicRemote().sendText(clientId + ":" + formatted);
+                    Thread.sleep(1000L);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         };
-        this.future = executorService.scheduleAtFixedRate(r, 0, 1, SECONDS);
+        thread = new Thread(new ClockRunnable(session, clientId));
+        thread.start();
     }
 
     @OnMessage
     public void stopClock(String message) {
         if ("stop".equals(message)) {
-            this.future.cancel(true);
+            this.thread.interrupt();
             System.out.println("Stopped..");
         }
     }
