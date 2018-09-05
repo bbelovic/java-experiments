4d1c63bdde64dd7631baf72bdf3a49e8ee595971
bbelovic <bbelovic@gmail.com>
2018-09-05 22:38:27 +0200

    simple cli app and playing with console.

---
+++ simple-websocket-client/src/main/java/net/bbelovic/javaexperiments/websocket/client/CliApp.java
@@ -0,0 +1,8 @@
+package net.bbelovic.javaexperiments.websocket.client;
+
+public class CliApp {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(System.console());
+    }
+}
